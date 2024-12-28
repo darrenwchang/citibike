a5b7b72e247cff6079e71ee7f9e8bcfcf9c46250
Darren Chang <darrenchang@MacBookAir.lan>
2024-12-28 16:10:42 -0500

    Add clustering example.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # citibike
+
+
+
+## TODO/ideas:
+map out price increase
+does this decrease memberships?
+does this change average ride length?
+what is price elasticity? map out per route? - this is revealed preferences
+ gravity model for each station/importance to each other
+ might have to use python instead of Julia
+ zoning data: https://data.cityofnewyork.us/City-Government/Georeferenced-NYC-Zoning-Maps/mxbm-493w/about_data
+ station info: https://gbfs.lyft.com/gbfs/2.3/bkn/en/station_information.json
--- import_citibike.jl
+++ import_citibike.jl
@@ -1,25 +1,42 @@
-using Downloads, ZipFile, DataFrames;
-import CSV;
+using Downloads, ZipFile, DataFrames, CSV
 
-url = "https://s3.amazonaws.com/tripdata/JC-202411-citibike-tripdata.csv.zip"
+function main(start_yr::Int, stop_yr::Int, jc::Bool  = false)
 
-io = IOBuffer()
-Downloads.download(url, io)
+    if jc 
+        prefix = "JC"
+        filename = "jc_data"
+    else 
+        prefix = ""
+        filename = "nyc_data" 
 
-r = ZipFile.Reader(io)
-
-for f in r.files
-    println(f.name)
-    if endswith(f.name,".csv")
-        df = DataFrame(CSV.Rows(f))
     end
+
+    df = DataFrame()
+
+    for yr in range(start_yr, stop=stop_yr)
+        for mon in range(1, stop=12)
+            month = lpad(mon, 2, "0")
+            year = string(yr)
+            
+            url = "https://s3.amazonaws.com/tripdata/$prefix-$year$month-citibike-tripdata.csv.zip"
+            println("processing $url...")
+
+            try
+                io = IOBuffer()
+                Downloads.download(url, io)
+
+                r = ZipFile.Reader(io)
+                df = vcat(df, DataFrame(CSV.Rows(r.files[1])))
+
+                catch
+                    println("error reading $url...")
+                end
+
+        end
+    end
+
+    CSV.write("$filename.csv", df)
 end
 
-for f in r.files
-    println("Filename: $(f.name)")
-    write(stdout, read(f, String));
- end
-df = DataFrame(CSV.Rows(r.files[2]))
-
-
-
+main(2023, 2024, true) # jc data
+# main(2023, 2024, false) # nyc data
